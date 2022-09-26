b835fdde6f7e2f395498dfa8a5caf5bbea605994
Matt Visnovsky <matt@visnovsky.us>
2022-09-26 16:24:55 -0600

    Missing "submit" text

--- lang/en.inc.php
+++ lang/en.inc.php
@@ -22,6 +22,7 @@ $messages['passwordrequired'] = "Please enter a password";
 $messages['search'] = "Search";
 $messages['searchrequired'] = "Please enter your search";
 $messages['specifyauth'] = "Please specify a valid authentication method in config.inc.php (currently \$auth_type=".((isset($auth_type) and !empty($auth_type))?$auth_type:"null").").";
+$messages['submit'] = "Submit";
 $messages['username'] = "Username";
 $messages['usernamerequired'] = "Please enter a username";
 $messages['usernotallowed'] = "Your account is not allowed to login. Please use an authorized account.";
