7654a1cd6b0f04b445f95efcd30a926380d58f1e
Matt Visnovsky <matt@visnovsky.us>
2023-02-07 16:22:09 -0700

    Revert "BUGFIX: goToPage() so you can 'reuse' constants"
    This reverts commit 6f85cde6329270ee3427102cffdce4e4428f5f52.

--- framework/js/examplepage.js
+++ framework/js/examplepage.js
@@ -2,5 +2,5 @@
     This example JS file demonstrates how you can load page-specific javacript from a TPL file,
     which will still be executed when the goToPage() function is ran.
 */
-const string = "This line of text demonstrates "+document.currentScript.src+" is being loaded.";
-console.log(string);
+
+console.log("This line of text demonstrates "+document.currentScript.src+" is being loaded.");
